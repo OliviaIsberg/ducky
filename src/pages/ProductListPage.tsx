4f45cf8e9062e7d1f0d0eb214b9c462a14354e74
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-09 14:32:27 +0100

    added container wrapper, placeholder img, and typography

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,3 +1,10 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Typography,
+} from '@mui/material'
 import { useState } from 'react'
 import { mockedProducts } from '../Api/Data'
 
@@ -5,13 +12,27 @@ function ProductListPage() {
   const [products] = useState(mockedProducts)
 
   return (
-    <div>
+    <Container maxWidth="md">
       {products.map((product) => (
-        <div key={product.id}>
-          <p>{product.title}</p>
-        </div>
+        <Card key={product.id}>
+          <CardContent>
+            <CardMedia
+              component="img"
+              height="240"
+              image={
+                'https://cdn.pixabay.com/photo/2017/10/05/22/34/rubber-duck-2821371_1280.jpg'
+              }
+            />
+            <Typography variant="h5" component="div">
+              {product.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {product.information}
+            </Typography>
+          </CardContent>
+        </Card>
       ))}
-    </div>
+    </Container>
   )
 }
 
