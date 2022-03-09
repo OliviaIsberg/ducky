2dba9ad2c841c754b1a96ab84bd9e26234c11898
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-09 15:27:53 +0100

    testing gridlayout

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -5,6 +5,7 @@ import {
   CardContent,
   CardMedia,
   Container,
+  Grid,
   Rating,
   Typography,
 } from '@mui/material'
@@ -13,34 +14,36 @@ import { mockedProducts } from '../Api/Data'
 
 function ProductListPage() {
   const [products] = useState(mockedProducts)
-  const [value] = useState(5)
+  const [ratingValue] = useState(5)
 
   return (
     <Container maxWidth="md">
-      {products.map((product) => (
-        <Card key={product.id}>
-          <CardContent>
-            <CardMedia
-              component="img"
-              height="240"
-              image={
-                'https://cdn.pixabay.com/photo/2017/10/05/22/34/rubber-duck-2821371_1280.jpg'
-              }
-            />
-            <Typography variant="h5" component="div">
-              {product.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {product.information}
-            </Typography>
-            <Rating name="read-only" value={value} readOnly />
-          </CardContent>
-          <CardActions>
-            <Button>Information</Button>
-            <Button variant="contained">Köp nu {product.price}kr</Button>
-          </CardActions>
-        </Card>
-      ))}
+      <Grid container sx={{ gap: '1rem' }}>
+        {products.map((product) => (
+          <Card key={product.id}>
+            <CardContent>
+              <CardMedia
+                component="img"
+                height="240"
+                image={
+                  'https://cdn.pixabay.com/photo/2017/10/05/22/34/rubber-duck-2821371_1280.jpg'
+                }
+              />
+              <Typography variant="h5" component="div">
+                {product.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {product.information}
+              </Typography>
+              <Rating name="read-only" value={ratingValue} readOnly />
+            </CardContent>
+            <CardActions>
+              <Button>Information</Button>
+              <Button variant="contained">Köp nu {product.price}kr</Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Grid>
     </Container>
   )
 }
