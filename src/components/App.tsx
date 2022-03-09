fc249f6f6708709f7eb210d640a98830c295ee23
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-09 11:10:02 +0100

    starting routing structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
+import ProductListPage from '../pages/ProductListPage'
+import Layout from './Layout'
+import StartPage from '../pages/StartPage'
+import TextPage from '../pages/TextPage'
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Startpage />} />
+        <Route index element={<StartPage />} />
         <Route path="productListPage" element={<ProductListPage />} />
-        <Route path="about" element={<Textpage />} />
+        <Route path="about" element={<TextPage />} />
       </Route>
     </Routes>
   )
