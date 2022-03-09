4fed2674473ba5f28d43ddd6d4c5cdf5c750dbba
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-09 14:14:56 +0100

    Pages > ProductListPage Get data and map through Fixes #27

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,5 +1,18 @@
-function ProductListPage (){
-    return(null)
+import { useState } from 'react'
+import { mockedProducts } from '../Api/Data'
+
+function ProductListPage() {
+  const [products] = useState(mockedProducts)
+
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          <p>{product.title}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default ProductListPage
