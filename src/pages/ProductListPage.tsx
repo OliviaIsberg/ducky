fb403fee97392066db787fab8153459a3942b855
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-14 13:19:04 +0100

    added icon on buy button, som e styling moved up props

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardActions,
@@ -9,43 +10,81 @@ import {
   Rating,
   Typography,
 } from '@mui/material'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { mockedProducts } from '../Api/Data'
+import { Product } from '../Api/Data'
 
-function ProductListPage() {
-  const [products] = useState(mockedProducts)
+interface Props {
+  productList: Product[]
+}
+
+function ProductListPage({ productList }: Props) {
+  const [products] = useState(productList)
   const [ratingValue] = useState(5)
 
   return (
     <Container maxWidth="md">
       <Grid container sx={{ gap: '1rem' }}>
-        {products.map((product) => (
-          <Card key={product.id}>
-            <CardContent>
-              <CardMedia
-                component="img"
-                height="240"
-                image={
-                  'https://cdn.pixabay.com/photo/2017/10/05/22/34/rubber-duck-2821371_1280.jpg'
-                }
-              />
-              <Typography variant="h5" component="div">
-                {product.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {product.information}
-              </Typography>
-              <Rating name="read-only" value={ratingValue} readOnly />
-            </CardContent>
-            <CardActions>
-              <Link to={`/products/${product.id}`}>
-                <Button>Information</Button>
-              </Link>
-              <Button variant="contained">Köp nu {product.price}kr</Button>
-            </CardActions>
-          </Card>
-        ))}
+        {products &&
+          products.map((product) => (
+            <Card
+              key={product.id}
+              sx={{ borderRadius: '1rem', padding: '1rem' }}
+            >
+              <CardContent sx={{ padding: '0' }}>
+                <CardMedia
+                  component="img"
+                  height="240"
+                  image={
+                    'https://cdn.pixabay.com/photo/2017/10/05/22/34/rubber-duck-2821371_1280.jpg'
+                  }
+                />
+                <Box
+                  component="div"
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    marginBlock: '1rem',
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    component="span"
+                    color="primary"
+                    fontWeight="700"
+                  >
+                    {product.title}
+                  </Typography>
+                  <Box component="span">
+                    <Rating name="read-only" value={ratingValue} readOnly />
+                  </Box>
+                </Box>
+                <Typography variant="body2" color="text.secondary" gutterBottom>
+                  {product.information}
+                </Typography>
+              </CardContent>
+              <CardActions
+                sx={{
+                  justifyContent: 'space-between',
+                  padding: '0',
+                  marginTop: '1rem',
+                }}
+              >
+                <Link to={`/products/${product.id}`}>
+                  <Button variant="outlined">Visa</Button>
+                </Link>
+                <Button
+                  onClick={() => console.log(product.title)}
+                  variant="contained"
+                  endIcon={<AddShoppingCartIcon />}
+                >
+                  Köp nu {product.price}kr
+                </Button>
+              </CardActions>
+            </Card>
+          ))}
       </Grid>
     </Container>
   )
