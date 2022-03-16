96d8753caeefd1331bd2865d1c04e7cfe2e5dc25
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-16 14:58:05 +0100

    visa produkter i en popover

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,53 @@
-import { Box, Tabs, Tab, Container, Badge } from '@mui/material'
+import {
+  Box,
+  Tabs,
+  Tab,
+  Container,
+  Badge,
+  Popover,
+  Button,
+  ListItem,
+  List,
+  Typography,
+  IconButton,
+  Tooltip,
+} from '@mui/material'
 import { FC, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle'
+
 import { useCart } from '../../contexts/ProductsInCartContext'
+import React from 'react'
+import { Product } from '../../Api/Data'
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
-  const { cart } = useCart()
+  const {
+    state: { cart },
+    dispatch,
+  } = useCart()
   let navigate = useNavigate()
   const [value, setValue] = useState('/')
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue)
     navigate(newValue)
   }
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+  const id = open ? 'simple-popover' : undefined
 
   return (
     <Container maxWidth="md" sx={{ marginBottom: '2rem' }}>
@@ -41,11 +73,63 @@ const Header: FC<HeaderProps> = () => {
         </Tabs>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
           <AccountCircleIcon color="action" />
-          <Link to="cartPage">
-            <Badge badgeContent={cart?.length} color="primary">
+          <Button
+            variant="contained"
+            aria-describedby={id}
+            onClick={handleClick}
+          >
+            Din kundkorg
+            <Badge badgeContent={cart?.length} color="info" showZero>
               <ShoppingCartIcon color="action" />
             </Badge>
-          </Link>
+          </Button>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <List>
+              {cart.length !== 0 ? (
+                cart.map((product: Product) => (
+                  <ListItem key={product.id}>
+                    <Typography variant="body2">{product.title}</Typography>
+                    <Tooltip title="Ta bort">
+                      <IconButton
+                        aria-label="delete"
+                        onClick={() => {
+                          dispatch({
+                            type: 'REMOVE_FROM_CART',
+                            payload: product,
+                          })
+                        }}
+                      >
+                        <RemoveCircleIcon color="error" />
+                      </IconButton>
+                    </Tooltip>
+                  </ListItem>
+                ))
+              ) : (
+                <Typography variant="body2">
+                  Du har inget i kundkorgen
+                </Typography>
+              )}
+            </List>
+
+            {cart.length !== 0 && (
+              <Link to="cartPage" onClick={handleClose}>
+                <Button variant="contained">Till kundkorgen</Button>
+              </Link>
+            )}
+          </Popover>
         </Box>
       </Box>
     </Container>
