0b4978d6110b295d844f842aeb411dac37a34a01
Olivia isberg <oliviaisberg@gmail.com>
2022-03-17 11:17:05 +0100

    Added questions in faqpage

--- src/pages/FaqPage.tsx
+++ src/pages/FaqPage.tsx
@@ -1,5 +1,41 @@
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 function FaqPage() {
-  return <h1>FAQ</h1>;
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <h3>Vad är kostar leveransen?</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          <p>
+            Leverans med Postnord kostar 19 kr, Scenker kostar 29 kr, Indstabox
+            kostar 29 kr.
+          </p>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <h3>När får jag min leverans?</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          <p>
+            Leverans med Postnord tar 1-3 dagar, Scenker tar 1-2 dagar, Instabox
+            tar 1-2 dagar
+          </p>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <h3>Vilka betalsätt kan jag välja?</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          <p>Våra betalmedel är Klarna, Swish, Visa, Maestro och MasterCard</p>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
 }
 
 export default FaqPage;
