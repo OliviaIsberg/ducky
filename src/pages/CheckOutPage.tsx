96de367c57123ee274314c5ac455b4bec3731490
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-03-18 12:38:10 +0100

    refactor checkoutpage

--- src/pages/CheckOutPage.tsx
+++ src/pages/CheckOutPage.tsx
@@ -1,16 +1,5 @@
-import {
-  Button,
-  Checkbox,
-  Container,
-  FormControlLabel,
-  Link,
-} from "@mui/material";
-import { Navigate, useNavigate } from "react-router-dom";
-import { placeOrderFetch } from "../Api/Api";
-import PaymentBox from "../components/Forms/PaymentBox";
-import ShipmentBox from "../components/Forms/ShipmentBox";
-import ShippingForm from "../components/Forms/ShippingForm";
-import ConfirmedOrderPage from "./ConfirmedPage";
+import { Container } from "@mui/material";
+import OrderForm from "../components/Forms/OrderForm";
 
 function onShippingSubmit() {}
 
@@ -19,44 +8,17 @@ function CheckOutPage() {
     <Container maxWidth="md">
       <h2>Kassa</h2>
       {/* < CartSummary /> */}
+
       <h3>Välj dina betal och leveransmetoder</h3>
+
       <p>
         Beställningar som görs innan kl 16.00 skickas samma dag. Ange uppgifter
         nedan för att se tillgängliga leveransval.
       </p>
-      <h3>Leveransadress</h3>
-      <ShippingForm onSubmit={onShippingSubmit} />
-      <h3>Leveransmetod</h3>
-      <ShipmentBox />
 
-      <h3>Betalningsmetod</h3>
-      <PaymentBox />
-      <h3>Betalningsdetaljer</h3>
-      {/* Newsletter checkbox, does nothing for now */}
-      <FormControlLabel
-        control={<Checkbox defaultChecked />}
-        label="Ja tack! Jag vill ha nyhetsbrev."
-      />
-
-      <FormControlLabel
-        control={<Checkbox />}
-        label="Jag godkänner köpvillkoren."
-      />
-      <Button variant="contained" onClick={() => confirmOrder()}>
-        Slutför beställning
-      </Button>
+      <OrderForm onSubmit={OrderData} />
     </Container>
   );
-}
-
-async function confirmOrder() {
-  // const success = await placeOrderFetch()
-  // let navigate = useNavigate();
-  // function handleClick() {
-  //   navigate('/confirmed-order')}
-  // if (success) {
-  //   return <Navigate replace = {true} to ='/confirmed-order' />
-  // }
 }
 
 export default CheckOutPage;
