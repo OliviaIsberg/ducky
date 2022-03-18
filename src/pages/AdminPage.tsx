76ecd95320640d26b7e088f966368441c71f93b2
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-18 13:47:40 +0100

    Set up link to adminpage
    Pages > AdminPage
    Fixes #20

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,4 +1,16 @@
-function AdminPage (){
+import { Button, Container } from '@mui/material'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+
+import { Link } from 'react-router-dom'
+
+function AdminPage() {
+  return (
+    <Container maxWidth="xl">
+      <Link to="/">
+        <Button startIcon={<ArrowBackIcon />}>Tillbaka till startsidan</Button>
+      </Link>
+    </Container>
+  )
 }
 
 export default AdminPage
