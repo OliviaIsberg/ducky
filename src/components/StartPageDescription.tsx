c4cf4208601177122b34b3cd5cd8ac2ba529f197
Olivia isberg <oliviaisberg@gmail.com>
2022-03-22 15:49:03 +0100

    Added logo and text to startPageDescription component

--- src/components/StartPageDescription.tsx
+++ src/components/StartPageDescription.tsx
@@ -1,5 +1,30 @@
+import { Box, Typography } from '@mui/material';
+import ducky from '../assets/ProductPictures/ducky2 1.png';
+
 function StartPageDesription() {
-  return <div>Hej</div>;
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '15rem',
+        background: 'lightpink',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <img style={{ width: '200px' }} src={ducky} alt=""></img>
+        <Typography sx={{ fontSize: 'clamp(1rem, 2.5vw, 1.5rem)' }}>
+          Välkommen till Ducky - affären där du köper dina badankor
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
 
 export default StartPageDesription;
