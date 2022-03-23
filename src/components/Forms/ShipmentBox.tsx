96a5bceac6f973e8ef1f90d352e745b20a532f6c
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-03-23 16:15:50 +0100

    Add delivery date Fixes #90

--- src/components/Forms/ShipmentBox.tsx
+++ src/components/Forms/ShipmentBox.tsx
@@ -5,7 +5,11 @@ import {
   ListItemText,
   ListItemAvatar,
   Avatar,
+  Typography,
 } from "@mui/material";
+import addDays from "date-fns/addDays";
+import format from "date-fns/format";
+import sv from "date-fns/locale/sv";
 import { FormikProps } from "formik";
 import React from "react";
 import { Delivery, deliveryOptions } from "../../Api/Data";
@@ -17,7 +21,7 @@ interface Props {
 
 function ShipmentBox(props: Props) {
   const [selectedIndex, setSelectedIndex] = React.useState("");
-  // const {deliveryOptions, deliveryDate, setDeliveryDate} = React.useState()
+
   const handleListItemClick = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>,
     index: string
@@ -26,36 +30,51 @@ function ShipmentBox(props: Props) {
     props.formikProps.setFieldValue("shippingMethod", index);
   };
 
-  // const date = new Date()
-  // const dateResult = new Date(date)
-  // dateResult.setDate(dateResult.getDate()+ deliveryOptions.shippingTime)
-  // setDeliveryDate(dateResult.toLocaleDateString())
-
-  // const calculateDeliveryCost=(
-
-  // )
-
   return (
     <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       <List component="nav" aria-label="Shipping options list">
         {deliveryOptions.length !== 0 &&
           deliveryOptions.map((delivery: Delivery) => (
-            <ListItemButton
-              key={delivery.id}
-              selected={selectedIndex === delivery.id}
-              onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) =>
-                handleListItemClick(event, delivery.id)
-              }
-            >
-              <ListItemAvatar>
-                <Avatar src={delivery.logo} alt={`${delivery.name} logo`} />
-              </ListItemAvatar>
+            <>
+              <ListItemButton
+                key={delivery.id}
+                selected={selectedIndex === delivery.id}
+                onClick={(
+                  event: React.MouseEvent<HTMLDivElement, MouseEvent>
+                ) => handleListItemClick(event, delivery.id)}
+              >
+                <ListItemAvatar>
+                  <Avatar src={delivery.logo} alt={`${delivery.name} logo`} />
+                </ListItemAvatar>
 
-              <ListItemText
-                primary={delivery.name}
-                secondary={`${delivery.altText} ${delivery.price} kr`}
-              />
-            </ListItemButton>
+                <ListItemText
+                  primary={delivery.name}
+                  secondary={
+                    <>
+                      <Typography
+                        sx={{ display: "block" }}
+                        component="span"
+                        variant="body2"
+                      >{`${delivery.altText}`}</Typography>
+                      <Typography
+                        sx={{ display: "block" }}
+                        component="span"
+                        variant="body2"
+                      >{`Leveranskostnad: ${delivery.price} kr`}</Typography>
+                      <Typography
+                        sx={{ display: "block" }}
+                        component="span"
+                        variant="body2"
+                      >{`Senaste datum för leverans: ${format(
+                        addDays(new Date(), delivery.shippingTime),
+                        "d MMMM",
+                        { locale: sv }
+                      )}`}</Typography>
+                    </>
+                  }
+                />
+              </ListItemButton>
+            </>
           ))}
       </List>
     </Box>
