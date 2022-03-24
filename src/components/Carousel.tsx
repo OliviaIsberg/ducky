34cca3938f43fe0cf97a49b15f73cbc3134ebb70
Olivia isberg <oliviaisberg@gmail.com>
2022-03-24 10:02:22 +0100

    Carousel is now responsive

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,9 +1,9 @@
-import { Container, Grid, Paper } from "@mui/material";
-import Carousel from "react-material-ui-carousel";
-import firstDuck from "../assets/CarouselPictures/firstDuck.jpg";
-import secondDuck from "../assets/CarouselPictures/secondDuck.jpg";
-import thirdDuck from "../assets/CarouselPictures/thirdDuck.jpg";
-import "../App.css";
+import { Container, Grid, Paper } from '@mui/material';
+import Carousel from 'react-material-ui-carousel';
+import firstDuck from '../assets/CarouselPictures/firstDuck.jpg';
+import secondDuck from '../assets/CarouselPictures/secondDuck.jpg';
+import thirdDuck from '../assets/CarouselPictures/thirdDuck.jpg';
+import '../App.css';
 
 interface Item {
   image: string;
@@ -27,11 +27,11 @@ function ShowCarousel() {
   ];
 
   return (
-    <Container maxWidth="xl" sx={{ padding: "4rem 0" }}>
+    <Container maxWidth="xl" sx={{ padding: '4rem 0' }}>
       <Carousel
         interval={2000}
         indicators={false}
-        sx={{ width: "90%", height: "700px", margin: "auto" }}
+        sx={{ width: '90%', height: '35vw', margin: 'auto' }}
       >
         {images.map((item, i) => (
           <ShowImages key={i} item={item} />
@@ -43,9 +43,9 @@ function ShowCarousel() {
 
 function ShowImages(props: Props) {
   return (
-    <Paper style={{ height: "700px", width: "100%" }}>
+    <Paper sx={{ height: '35vw', width: '100%' }}>
       <img
-        style={{ height: "100%", width: "100%" }}
+        style={{ height: '100%', width: '100%' }}
         src={props.item.image}
         alt=""
       />
