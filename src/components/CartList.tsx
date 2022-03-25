d87532572a3441fd4c73fd5b886127bcd5edddac
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-25 15:48:10 +0100

    Extracted cart list to component

---
+++ src/components/CartList.tsx
@@ -0,0 +1,144 @@
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  ButtonGroup,
+  Button,
+  ListItemIcon,
+  Tooltip,
+  IconButton,
+  Typography,
+  Divider,
+  Box,
+} from '@mui/material'
+import PaymentIcon from '@mui/icons-material/Payment'
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle'
+import RemoveIcon from '@mui/icons-material/Remove'
+import AddIcon from '@mui/icons-material/Add'
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useCart } from '../contexts/CartContext'
+import { CartType, Types } from '../contexts/Reducers'
+import useLocalStorage from '../Hooks/useLocalStorage'
+
+function CartList() {
+  const { cart, dispatch } = useCart()
+  const [total, setTotal] = useLocalStorage('cartSum', 0)
+
+  useEffect(() => {
+    setTotal(
+      cart.reduce(
+        (acc: number, current: CartType) => acc + current.price * current.qty,
+        0
+      )
+    )
+  }, [cart, setTotal])
+
+  return (
+    <>
+      <List>
+        {cart && cart.length > 0 ? (
+          cart.map((product: CartType) => (
+            <ListItem key={product.id}>
+              <ListItemAvatar>
+                <img
+                  src={product.imgURL}
+                  alt={product.title}
+                  style={{
+                    width: '70px',
+                    height: '70px',
+                    borderRadius: '50%',
+                  }}
+                />
+              </ListItemAvatar>
+              <ListItemText
+                primary={product.title}
+                secondary={`${product.price} kr/st`}
+                sx={{ marginLeft: '.5rem' }}
+              />
+              <ButtonGroup
+                size="small"
+                sx={{ flexGrow: '1', justifyContent: 'flex-end' }}
+              >
+                <Button
+                  onClick={() => {
+                    dispatch({
+                      type: Types.UpdateQty,
+                      payload: {
+                        id: product.id,
+                        qty: (product.qty = product.qty - 1),
+                      },
+                    })
+                  }}
+                >
+                  <RemoveIcon />
+                </Button>
+                <Button disableRipple>{product.qty}</Button>
+                <Button
+                  onClick={() => {
+                    dispatch({
+                      type: Types.UpdateQty,
+                      payload: {
+                        id: product.id,
+                        qty: (product.qty += 1),
+                      },
+                    })
+                  }}
+                >
+                  <AddIcon />
+                </Button>
+              </ButtonGroup>
+
+              <ListItemText sx={{ textAlign: 'right' }}>
+                {product.price * product.qty} kr
+              </ListItemText>
+              <ListItemIcon>
+                <Tooltip title="Ta bort">
+                  <IconButton
+                    aria-label="delete"
+                    edge="end"
+                    onClick={() => {
+                      dispatch({
+                        type: Types.DeleteFromCart,
+                        payload: product,
+                      })
+                    }}
+                  >
+                    <RemoveCircleIcon color="error" />
+                  </IconButton>
+                </Tooltip>
+              </ListItemIcon>
+            </ListItem>
+          ))
+        ) : (
+          <Typography variant="body1">Här var det tomt!</Typography>
+        )}
+      </List>
+      <Divider light textAlign="right">
+        Summa
+      </Divider>
+      <Box maxWidth="md" sx={{ paddingInline: '1rem', textAlign: 'right' }}>
+        <Typography variant="h6" textAlign="right" sx={{ mb: 10 }}>
+          {total} kr
+        </Typography>
+        <Link to="/products">
+          <Button variant="contained" sx={{ mr: 2 }}>
+            Fortsätt handla
+          </Button>
+        </Link>
+        <Link to={cart.length ? '/checkoutPage' : ''}>
+          <Button
+            variant="outlined"
+            endIcon={<PaymentIcon />}
+            disabled={cart.length > 0 ? false : true}
+          >
+            Till betalning
+          </Button>
+        </Link>
+      </Box>
+    </>
+  )
+}
+
+export default CartList
--- src/components/Header/CartButton.tsx
+++ src/components/Header/CartButton.tsx
@@ -1,24 +1,12 @@
-import {
-  Button,
-  Badge,
-  Popover,
-  List,
-  ListItem,
-  Typography,
-  Tooltip,
-  IconButton,
-  ListItemText,
-  ListItemAvatar,
-} from '@mui/material'
+import { Button, Badge, Popover, Typography } from '@mui/material'
 import React from 'react'
-import RemoveCircleIcon from '@mui/icons-material/RemoveCircle'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import { Link } from 'react-router-dom'
 import { useCart } from '../../contexts/CartContext'
-import { CartType, Types } from '../../contexts/Reducers'
+import CartList from '../CartList'
 
 function CartButton() {
-  const { cart, dispatch } = useCart()
+  const { cart } = useCart()
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -54,45 +42,11 @@ function CartButton() {
           horizontal: 'right',
         }}
       >
-        <List dense>
-          {cart && cart.length > 0 ? (
-            cart.map((product: CartType) => (
-              <ListItem key={product.id}>
-                <ListItemAvatar>
-                  <img
-                    src={product.imgURL}
-                    alt={product.title}
-                    style={{
-                      width: '40px',
-                      height: '40px',
-                      borderRadius: '50%',
-                    }}
-                  />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={product.title}
-                  secondary={`${product.price} kr/st`}
-                />
-                <Tooltip title="Ta bort">
-                  <IconButton
-                    aria-label="delete"
-                    edge="end"
-                    onClick={() => {
-                      dispatch({
-                        type: Types.DeleteFromCart,
-                        payload: product,
-                      })
-                    }}
-                  >
-                    <RemoveCircleIcon color="error" />
-                  </IconButton>
-                </Tooltip>
-              </ListItem>
-            ))
-          ) : (
-            <Typography variant="body2">Du har inget i kundkorgen</Typography>
-          )}
-        </List>
+        {cart?.length > 0 ? (
+          <CartList />
+        ) : (
+          <Typography variant="body2">Du har inget i kundkorgen</Typography>
+        )}
 
         {cart.length > 0 && (
           <Link to="cartPage" onClick={handleClose}>
