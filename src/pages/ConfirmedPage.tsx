8cf3f029bf295c08dd8f5a93b4feb8c4e7ba0d38
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-25 15:03:55 +0100

    list style on products

--- src/pages/ConfirmedPage.tsx
+++ src/pages/ConfirmedPage.tsx
@@ -1,19 +1,27 @@
-import { Container, Divider } from "@mui/material";
-import { deliveryOptions, paymentOptions } from "../Api/Data";
-import { OrderData } from "../components/Forms/OrderForm";
-import { CartType } from "../contexts/Reducers";
-import useLocalStorage from "../Hooks/useLocalStorage";
+import {
+  Container,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from '@mui/material'
+import { deliveryOptions, paymentOptions } from '../Api/Data'
+import { OrderData } from '../components/Forms/OrderForm'
+import { CartType } from '../contexts/Reducers'
+import useLocalStorage from '../Hooks/useLocalStorage'
 
 // randomizes a 6 digit order number
 function RandomOrderNumber() {
-  return Math.floor(Math.random() * 1000000);
+  return Math.floor(Math.random() * 1000000)
 }
 
 function ConfirmedOrderPage() {
   // get cart, total cartsum, all orderdetails and shippingdetails from local storage
-  const [cart] = useLocalStorage<CartType[]>("cart", "");
-  const [total] = useLocalStorage<number>("cartSum", 0);
-  const [orderDetails] = useLocalStorage<OrderData>("orderDetails", "");
+  const [cart] = useLocalStorage<CartType[]>('cart', '')
+  const [total] = useLocalStorage<number>('cartSum', 0)
+  const [orderDetails] = useLocalStorage<OrderData>('orderDetails', '')
 
   return (
     <Container maxWidth="md">
@@ -28,21 +36,44 @@ function ConfirmedOrderPage() {
       <h3>Ordernummer: #{RandomOrderNumber()}</h3>
       <h3>Produkter:</h3>
       {/* get the summary of bought products, loops thought cart array */}
-      {cart?.length &&
-        cart.map((c: CartType) => (
-          <p key={c.id}>
-            Produkt: {c.title} Antal: {c.qty} Pris: {c.price}/st
-          </p>
-        ))}
+      <List dense>
+        {cart?.length &&
+          cart.map((c: CartType) => (
+            <ListItem key={c.id}>
+              <ListItemAvatar>
+                <img
+                  src={c.imgURL}
+                  alt={c.title}
+                  style={{
+                    width: '70px',
+                    height: '70px',
+                    borderRadius: '50%',
+                  }}
+                />
+              </ListItemAvatar>
+              <ListItemText
+                primary={c.title}
+                secondary={`Antal: ${c.qty} Pris: ${c.price} kr/st`}
+              />
+              <ListItemText
+                primary={`${c.qty * c.price} kr`}
+                sx={{ textAlign: 'right' }}
+              />
+            </ListItem>
+          ))}
+      </List>
+      <Divider />
       {/* get and print total price of products */}
       {/* the second "total" should be shipping cost */}
-      Totalpris (inkl moms & frakt) : {`${total}`} kr
-      <Divider />
+      <Typography variant="body1" sx={{ textAlign: 'right' }}>
+        Totalpris (inkl moms & frakt) : {`${total}`} kr
+      </Typography>
+      {/* Totalpris (inkl moms & frakt) : {`${total}`} kr */}
       {/* Get shipping adress from local storage  */}
       <h3>Leveransadress:</h3>
       {/* first and last name */}
       <>
-        {orderDetails.shippingAdress.firstName}{" "}
+        {orderDetails.shippingAdress.firstName}{' '}
         {orderDetails.shippingAdress.lastName}
       </>
       <br />
@@ -51,7 +82,7 @@ function ConfirmedOrderPage() {
       <br />
       {/* post code and city */}
       <>
-        {orderDetails.shippingAdress.postCode}{" "}
+        {orderDetails.shippingAdress.postCode}{' '}
         {orderDetails.shippingAdress.city}
       </>
       <br />
@@ -65,23 +96,25 @@ function ConfirmedOrderPage() {
       {/* Get shipping method from local storage  */}
       <h3>Leveransmetod:</h3>
       <>
-        {typeof orderDetails.shippingMethod === "number"
+        {typeof orderDetails.shippingMethod === 'number'
           ? deliveryOptions[orderDetails.shippingMethod].name
-          : ""}
+          : ''}
       </>
       <Divider />
       {/* Get payment method from local storage  */}
       <h3>Betalningsmetod:</h3>
-      <>{typeof orderDetails.paymentMethod === "number"
+      <>
+        {typeof orderDetails.paymentMethod === 'number'
           ? paymentOptions[orderDetails.paymentMethod].name
-          : ""}</>
+          : ''}
+      </>
       <Divider />
       <p>
         Skulle någonting inte stämma, eller om du har övriga frågor är du varmt
         välkommen att kontakta oss på: support@ducky.se
       </p>
     </Container>
-  );
+  )
 }
 
-export default ConfirmedOrderPage;
+export default ConfirmedOrderPage
