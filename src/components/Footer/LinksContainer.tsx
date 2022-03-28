8c7b61c7af96bcc5ac507bd6b343b4d78bea00aa
Olivia isberg <oliviaisberg@gmail.com>
2022-03-28 23:15:34 +0200

    Added more styling to footer and added timeout on email input field

--- src/components/Footer/LinksContainer.tsx
+++ src/components/Footer/LinksContainer.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
+import { createTheme } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
+import { ThemeProvider } from '@emotion/react';
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import klarna from '../../assets/PaymentLogos/klarna.png';
@@ -12,59 +15,118 @@ import instabox from '../../assets/ShippingLogos/instabox-full.jpg';
 import postnord from '../../assets/ShippingLogos/postnord-full.png';
 import schenker from '../../assets/ShippingLogos/schenker-full.png';
 import SubscribeForm from '../Forms/SubscribeForm';
+import duckLogo from '../../assets/ducky2 1.png';
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#1a237e',
+    },
+  },
+  typography: {
+    fontSize: 15,
+  },
+});
+
+const iconSize = createTheme({
+  palette: {
+    secondary: {
+      main: '#ec407a',
+    },
+    primary: {
+      main: '#3d5afe',
+    },
+  },
+  typography: {
+    fontSize: 22,
+  },
+});
 
 function LinksContainer() {
   return (
     <Grid
       container
-      spacing={3}
-      sx={{ margin: 'auto', width: '70%', padding: '3rem 0' }}
+      spacing={2}
+      sx={{ margin: 'auto', width: '90%', padding: '3rem 0' }}
     >
       <Grid
         item
         xs={12}
-        md={4}
-        lg={2}
-        sx={{ display: 'flex', justifyContent: 'center' }}
-      >
-        <Box>
-          <Typography gutterBottom variant="h6">
-            Hjälp
-          </Typography>
-          <Typography gutterBottom variant="body1">
-            <Link to="faq">Vanliga frågor</Link>
-          </Typography>
-          <Typography gutterBottom variant="body1">
-            <Link to="termsOfUse">Användarvillkor</Link>
-          </Typography>
-          <Typography variant="body1">
-            <Link to="support">Kundtjänst</Link>
-          </Typography>
-        </Box>
-      </Grid>
-      <Grid
-        item
-        xs={11}
-        md={4}
+        md={3}
         lg={2}
         sx={{
           display: 'flex',
           justifyContent: 'center',
         }}
       >
-        <Box>
+        <img src={duckLogo} width="200" height="200" alt=""></img>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        md={3}
+        lg={2}
+        sx={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <Box sx={{ textAlign: 'center' }}>
           <Typography gutterBottom variant="h6">
-            Följ oss
+            Hjälp
           </Typography>
-          <FacebookOutlinedIcon color="primary" fontSize="large" />
-          <InstagramIcon color="secondary" fontSize="large" />
+          <ThemeProvider theme={theme}>
+            <Typography gutterBottom>
+              <Link to="faq">
+                <Button color="secondary" variant="text">
+                  Vanliga frågor
+                </Button>
+              </Link>
+            </Typography>
+            <Typography gutterBottom>
+              <Link to="termsOfUse">
+                <Button color="secondary" variant="text">
+                  Användarvillkor
+                </Button>
+              </Link>
+            </Typography>
+            <Typography>
+              <Link to="support">
+                <Button color="secondary" variant="text">
+                  Kundtjänst
+                </Button>
+              </Link>
+            </Typography>
+          </ThemeProvider>
         </Box>
       </Grid>
       <Grid
         item
         xs={12}
-        md={4}
-        lg={4}
+        md={3}
+        lg={2}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Typography gutterBottom variant="h6">
+            Följ oss
+          </Typography>
+          <ThemeProvider theme={iconSize}>
+            <FacebookOutlinedIcon color="primary" fontSize="large" />
+            <InstagramIcon color="secondary" fontSize="large" />
+          </ThemeProvider>
+        </Box>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        md={3}
+        lg={2}
         sx={{ display: 'flex', justifyContent: 'center' }}
       >
         <Box>
