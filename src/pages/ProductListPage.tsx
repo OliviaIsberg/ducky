5381a7935d4e096437574240dd0747018c09573d
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-03-29 14:41:06 +0200

    more responsive grid

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,18 +1,18 @@
 import { Container, Grid } from '@mui/material'
 import ProductCard from '../components/Cards/ProductCard'
-import FilterBar from '../components/FilterBar'
 import { useProduct, ProductType } from '../contexts/ProductsContext'
 
 function ProductListPage() {
   const { products } = useProduct()
 
   return (
-    <Container maxWidth="md">
-      <FilterBar />
-      <Grid container sx={{ gap: '1rem' }}>
+    <Container maxWidth="lg">
+      <Grid container spacing={2}>
         {products &&
           products.map((product: ProductType) => (
-            <ProductCard key={product.id} product={product} />
+            <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard key={product.id} product={product} />
+            </Grid>
           ))}
       </Grid>
     </Container>
