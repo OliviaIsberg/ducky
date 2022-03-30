c78c7aec0cc2bb144518c748771c7644f3842498
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-03-30 10:41:59 +0200

    fixed main (flex grow)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,17 @@
+import { Container } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer/Footer'
 import Header from './Header/Header'
 
 function Layout() {
   return (
-    <div style={{ height: '100%' }}>
+    <Container maxWidth={false} disableGutters={true} sx={{ display:"flex", flexDirection:"column", height:"100%"}}>
       <Header />
+      <Container component="main" disableGutters={true} sx={{flexGrow:"1"}}>
       <Outlet />
+      </Container>
       <Footer />
-    </div>
+    </Container>
   )
 }
 
